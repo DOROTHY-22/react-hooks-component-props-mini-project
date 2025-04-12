cd49ef9a261f94986b10b858f414b1e5f3fb0819
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-04-12 16:57:18 +0300

    Add changes made

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,57 @@ import React from "react";
 import blogData from "../data/blog";
 
 console.log(blogData);
-
+function Article(props){
+  const {title,date,preview} =props;
+  const defaultDate = "January 1, 1970"
+  return(
+    <article>
+      <h3>{title}</h3>
+      <small>{date || defaultDate}</small>
+      <p>{preview}</p>
+    </article>
+  )
+}
+function ArticleList(props){
+  const {posts} = props;
+  return(
+    <main>
+      {posts.map((post)=>(
+      <Article
+       id={post.id}
+       title={post.title}
+       date={post.date}
+       preview={post.preview}
+       minutes={post.minutes}/>
+    
+      ))}
+    
+    </main>
+  )
+}
+function About(props){
+  const { image, about }=props
+  const defaultImage = "https://via.placeholder.com/215"
+  return(
+    <aside>
+      <img src={image || defaultImage}alt="blog logo"/>
+      <p>{about}</p>
+    </aside>
+  )
+}
+function Header(props){
+  return(
+    <header>
+      <h1>{props.name}</h1>
+    </header>
+  )
+}
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about}/>
+      <ArticleList posts={blogData.posts}/>
     </div>
   );
 }
